df6e5dc9d1efc7d49b3e6641a9f5b7429c9526de
Victor Emanuel <victor.emanuel@esig.com.br>
2021-02-27 18:40:52 -0300

    SendMail controller and repository
    add new route: show users

--- API/src/controllers/SurveysController.ts
+++ API/src/controllers/SurveysController.ts
@@ -4,25 +4,18 @@ import { SurveysRepository } from '../repositories/SurveysRepository';
 
 class SurveysController {
     async create(request: Request, response: Response) {
-
         const { title, description } = request.body;
-
         const surveysRepository = getCustomRepository(SurveysRepository);
-
         const survey = surveysRepository.create({
             title, description
         });
-
         await surveysRepository.save(survey);
-
         return response.status(201).json(survey);
     }
 
     async show(request: Request, response: Response) {
         const surveysRepository = getCustomRepository(SurveysRepository);
-
         const all = await surveysRepository.find();
-
         return response.json(all);
     }
 }
--- API/src/routes.ts
+++ API/src/routes.ts
@@ -1,14 +1,20 @@
 import { Router } from 'express';
 import { UserController } from './controllers/UserController';
 import { SurveysController } from './controllers/SurveysController';
+import { SendMailController } from './controllers/SendMailController';
 
 const router = Router();
 
 const userController = new UserController();
 const surveysController = new SurveysController();
+const sendMailController = new SendMailController();
 
 router.post('/users', userController.create);
+router.get('/users', userController.show);
+
 router.post('/surveys', surveysController.create);
 router.get('/surveys', surveysController.show);
+
+router.post('/send_mail', sendMailController.execute);
 
 export { router };
